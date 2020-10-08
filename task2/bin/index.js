de854111d9d3666125873dc661bec0aa0b59575c
Volha_Mashnitskaya <volha_mashnitskaya@epam.com>
2020-10-08 16:32:06 +0300

    add streams

--- task2/bin/index.js
+++ task2/bin/index.js
@@ -1,12 +1,37 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
+const path = require('path');
+const fs = require('fs');
+const {pipeline} = require('stream');
 
 program
-    .option('-s, --shift', 'shift in cipher')
-    .option('-i, --input', 'input file')
-    .option('-o, --output', 'output file')
-    .option('-a, --action', 'action(encode/decode)')
+    .option('-s, --shift <shift>', 'shift in cipher')
+    .option('-i, --input <inputFile>', 'input file')
+    .option('-o, --output <outputFile>', 'output file')
+    .option('-a, --actions <actionToPerform>', 'action(encode/decode)')
     .parse(process.argv);
 
-console.log('Hello');
+
+
+process.on('exit', (code) => {
+    console.log(`Exited with code: ${code}`);
+});
+
+if(!program.shift || !program.actions){
+    console.log('Parameters --shift and --actions are required. Please try again.')
+    process.exit(400);
+}
+
+const inputFilePath = path.resolve(__dirname, '..', `files/${program.input}`);
+const outputFilePath = path.resolve(__dirname, '..', `files/${program.output}`);
+
+
+pipeline(
+    fs.createReadStream(inputFilePath),
+    fs.createWriteStream(outputFilePath),
+    (error) =>{
+        console.log(error);
+    }
+)
+
